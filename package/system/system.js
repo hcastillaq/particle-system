bdf16b282bae394af37c0f2c2bac37e05ed0b354
hernan castilla <hcastillaq@gmail.com>
2023-02-09 12:01:46 -0500

    update tsconfig

--- package/system/system.js
+++ package/system/system.js
@@ -1,1 +1,82 @@
-import{BufferGeometry,Float32BufferAttribute,Scene}from"three";export class ParticleSystem{constructor(){this.particles=[],this.numberParticles=0,this.speed=1,this.geometry=new BufferGeometry,this.scene=new Scene}setParticlesNumber(e){this.numberParticles=e}getParticlesNumber(){return this.particles.length}setGeometry(e){this.geometry=e,this.makeVertices()}setSpeed(e){this.speed=e}changeSpeed(e){this.speed=e}makeVertices(){const e=[];for(let t=0;t<this.numberParticles;t++){const t=this.make();this.particles.push(t),e.push(t.x,t.y,t.z)}this.updateGeometricParticles(e)}updateGeometricParticles(e){this.geometry.setAttribute("position",new Float32BufferAttribute(e,3))}random(e,t){return Math.random()*(t-e)+e}dispose(){this.geometry.dispose(),this.geometry.deleteAttribute("position"),this.particles=[],this.numberParticles=0,this.speed=1}setScene(e){this.scene=e}apply(e,t,s,i){this.geometry.attributes.position.setXYZ(e,t,s,i)}updateParticles(){const e=[];for(let t=0;t<this.particles.length;t++){const s=this.particles[t];e.push(s.x,s.y,s.z)}this.updateGeometricParticles(e)}validatePositiveNumber(e){return e>=0}changeNumberParticles(e){if(e=Number(e.toPrecision(1)),this.validatePositiveNumber(e)&&(e<this.numberParticles&&(this.particles=this.particles.splice(this.numberParticles-e),this.setParticlesNumber(this.particles.length),this.updateParticles()),e>this.numberParticles)){for(let t=0;t<e;t++)this.particles.push(this.make());this.setParticlesNumber(this.numberParticles+e),this.updateParticles()}}}
+import { BufferGeometry, Float32BufferAttribute, Scene } from "three";
+export class ParticleSystem {
+    particles = [];
+    numberParticles = 0;
+    speed = 1;
+    geometry = new BufferGeometry();
+    scene = new Scene();
+    setParticlesNumber(number) {
+        this.numberParticles = number;
+    }
+    getParticlesNumber() {
+        return this.particles.length;
+    }
+    setGeometry(geometry) {
+        this.geometry = geometry;
+        this.makeVertices();
+    }
+    setSpeed(speed) {
+        this.speed = speed;
+    }
+    changeSpeed(speed) {
+        this.speed = speed;
+    }
+    makeVertices() {
+        const vertices = [];
+        for (let i = 0; i < this.numberParticles; i++) {
+            const particle = this.make();
+            this.particles.push(particle);
+            vertices.push(particle.x, particle.y, particle.z);
+        }
+        this.updateGeometricParticles(vertices);
+    }
+    updateGeometricParticles(vertices) {
+        this.geometry.setAttribute("position", new Float32BufferAttribute(vertices, 3));
+    }
+    random(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+    dispose() {
+        this.geometry.dispose();
+        this.geometry.deleteAttribute("position");
+        this.particles = [];
+        this.numberParticles = 0;
+        this.speed = 1;
+    }
+    setScene(scene) {
+        this.scene = scene;
+    }
+    apply(index, x, y, z) {
+        const position = this.geometry.attributes.position;
+        position.setXYZ(index, x, y, z);
+    }
+    updateParticles() {
+        const vertices = [];
+        for (let i = 0; i < this.particles.length; i++) {
+            const particle = this.particles[i];
+            vertices.push(particle.x, particle.y, particle.z);
+        }
+        this.updateGeometricParticles(vertices);
+    }
+    validatePositiveNumber(value) {
+        return value >= 0 ? true : false;
+    }
+    changeNumberParticles(size) {
+        size = Number(size.toPrecision(1));
+        if (this.validatePositiveNumber(size)) {
+            if (size < this.numberParticles) {
+                this.particles = this.particles.splice(this.numberParticles - size);
+                this.setParticlesNumber(this.particles.length);
+                this.updateParticles();
+            }
+            if (size > this.numberParticles) {
+                for (let i = 0; i < size; i++) {
+                    this.particles.push(this.make());
+                }
+                this.setParticlesNumber(this.numberParticles + size);
+                this.updateParticles();
+            }
+        }
+    }
+}
+//# sourceMappingURL=system.js.map
--- src/attractors/chen.attractor.ts
+++ src/attractors/chen.attractor.ts
@@ -1,4 +1,4 @@
-import { ParticleSystem } from "../lib/system/system";
+import { ParticleSystem } from "particle-system";
 
 class ChenAttractor extends ParticleSystem {
 	numberParticles = 1000000;
